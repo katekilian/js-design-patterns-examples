abbc3e9b68b69d5a02de1368db4c98813fc202cf
mijikim <miji.kim926@gmail.com>
2016-09-17 16:24:58 -0700

    created our coffee and tea classes, created DrinkFactory, instantiated and tested

---
+++ factory-pattern/index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var Coffee = function(options) {
+  this.size = options.size || "small";
+  this.temperature = options.temperature || "hot";
+  this.sweetened = options.sweetened || false;
+  this.quantity = options.quantity || 1;
+};
+
+var Tea = function(options) {
+  this.teaType = options.teaType || "black tea";
+  this.sweetened = options.sweetened || true;
+  this.sweetenerType = options.sweetenerType || "honey";
+  this.quantity = options.quantity || 2;
+};
+
+var DrinkFactory = function() {};
+
+DrinkFactory.prototype.drinkType = Coffee;
+
+DrinkFactory.prototype.createDrink = function(options) {
+  switch(options.drinkType) {
+    case "Coffee":
+      this.drinkType = Coffee;
+      break;
+    case "Tea":
+      this.drinkType = Tea;
+      break;
+  }
+
+  return new this.drinkType(options);
+};
+
+var coffeeFactory = new DrinkFactory();
+
+var nonDefaultsCoffee = coffeeFactory.createDrink({
+  drinkType: "Coffee",
+  size: "medium", 
+  temperature: "lukewarm", 
+  sweetened: true
+});
+
+console.log(nonDefaultsCoffee);
+console.log(nonDefaultsCoffee instanceof Coffee);
+
